62b86cda06888bf9169a396c3b44a0e70a74f4a6
Blaser993 <corradodepinto.design@gmail.com>
2023-02-09 14:30:07 +0100

    START

---
+++ script/campominato.js
@@ -0,0 +1,8 @@
+console.log("BOOM!");
+
+// Oggi concentriamoci solamente sulla creazione della griglia:
+// l’utente clicca su un pulsante PLAY  per generare la una griglia quadrata 10 * 10
+// ogni casella deve contenere il numero corrispondente a partire da 1 fino a 100
+//  2.  Ogni volta che l’utente clicca su una delle caselle della griglia stampiamo in console il numero della casella cliccata (se ci riusciamo, in caso contrario stampiamo un messaggio generico)
+// BONUS
+// quando una casella viene cliccata oltre al console log del numero aggiungiamole una classe che ne cambi il color di background
